aa681b76c3cb002a81a835f4e5955f7b4f50afef
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-21 20:02:54 +0900

    feat: 상속 예제

---
+++ class/class-extends-instances.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-classes-per-file */
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+
+  toString() {
+    return `width = ${this.width}, height = ${this.height}`;
+  }
+}
+
+class ColorRectangle extends Rectangle {
+  constructor(width, height, color) {
+    super(width, height);
+    this.color = color;
+  }
+
+  toString() {
+    return `${super.toString()}, color = ${this.color}`;
+  }
+}
+
+const colorRectangle = new ColorRectangle(2, 4, 'red');
+console.log(colorRectangle); // ColorRectangle { width: 2, height: 4, color: 'red' }
+
+// 상속을 통해 getArea 메서드를 호출
+console.log(colorRectangle.getArea()); // 8
+// 오버라이딩된 toString 메서드를 호출
+console.log(colorRectangle.toString()); // width = 2, height = 4, color = red
